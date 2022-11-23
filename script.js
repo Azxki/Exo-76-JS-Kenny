a1dbb2ea00a1acec2a2a9b47e84e430e6c6fdac1
azxki <cococoursan@gmail.com>
2022-11-23 14:45:03 +0100

    EXO

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
-
-
-document.getElementById('up').addEventListener('click',function() {
+document.getElementById('up').addEventListener('click',function () {
     let t = parseInt(document.getElementById('kenny').style.top);
-    t = t - 10;
+    if (t > 0) {
+        t = t - 10;
+    }
     document.getElementById('kenny').style.top = t + 'px';
 });
+
+document.getElementById('left').addEventListener('click',function () {
+    let t = parseInt(document.getElementById('kenny').style.left);
+    if (t > 20) {
+        t = t - 10;
+    }
+    else {
+        alert("Kenny est mort !")
+        t = 200;
+    }
+
+    document.getElementById('kenny').style.left = t + 'px';
+
+});
+
+document.getElementById('right').addEventListener('click',function() {
+    let t = parseInt(document.getElementById('kenny').style.left);
+    if (t < 470) {
+        t = t + 10;
+    }
+    document.getElementById('kenny').style.left = t + 'px';
+});
+
+document.getElementById('down').addEventListener('click',function() {
+    let t = parseInt(document.getElementById('kenny').style.top);
+        if (t < 470) {
+           t = t + 10;
+        }
+    document.getElementById('kenny').style.top = t + 'px';
+});
+
